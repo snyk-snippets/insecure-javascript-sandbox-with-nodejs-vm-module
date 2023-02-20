94c68887aa4b4fda6da60c996fa150cac513d9fa
Liran Tal <liran.tal@gmail.com>
2023-02-20 09:52:03 +0200

    feat: a rudimantery example for vm module usage

---
+++ vm1.js
@@ -0,0 +1,20 @@
+const vm = require("node:vm");
+
+const productExpirationDays = 14;
+
+// This works just fine - it is a custom code that manipulates
+// the data in the context and only there.
+const userInputCustomJavaScriptCode = "userCustomNickname = 'Johnny Mnemonic';";
+
+// This code will throw an error because there's no such variable
+// defined in the context as productExpirationMinutes.
+// const userInputCustomJavaScriptCode = "userCustomNickname = 'Johnny Mnemonic'; productExpirationMinutes += 60";
+
+const context = { userCustomNickname: "John Nash" };
+vm.createContext(context);
+
+vm.runInContext(userInputCustomJavaScriptCode, context);
+
+console.log(context.userCustomNickname);
+console.log(context.productExpirationDays);
+console.log(productExpirationDays);
